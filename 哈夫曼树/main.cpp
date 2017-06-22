ccda35c5f3ced1f704d55c08c874e8754c543ed9
HTWER <1158261439@qq.com>
2017-06-22 13:45:04 +0800

    最终解决乱码问题

--- 哈夫曼树/main.cpp
+++ 哈夫曼树/main.cpp
@@ -1,8 +1,8 @@
-#include "HuffmanTree.h"
+ï»¿#include "HuffmanTree.h"
 
 int main()
 {
-	HuffmanTree test;		//²âÊÔÊı¾İ"4 a 2 b 3 c 4 d 5"
+	HuffmanTree test;		//æµ‹è¯•æ•°æ®"4 a 2 b 3 c 4 d 5"
 
 	cout << endl;
 	cout << test;
